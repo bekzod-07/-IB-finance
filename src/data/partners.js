10a13c8e90f5a744e5d51d1c109b74b7548cabc3
bekzod-07 <bekzodsindorov0@gmail.com>
2026-05-01 19:14:53 +0500

    partners

--- src/data/partners.js
+++ src/data/partners.js
@@ -1,1 +1,1 @@
-export const partners = ['Alpha Group','Green Line','Navo Finance','Prime Trade','Eco Build','Smart Retail'];
+export const partners = ['Water Plant Building','Lexcell','Yangi Zamon Project','Davlatbek Zig\'ir osh'];
